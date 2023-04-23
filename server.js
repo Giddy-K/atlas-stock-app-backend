8ac8c8e829d348306bc593e7fae79fc527325afa
Giddy-K <gideonkipamet@gmail.com>
2023-04-24 00:47:19 +0300

    server changes

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://atlas-app.vercel.app"],
+    origin: ["http://localhost:3000", "https://atlas-stock-app.vercel.app"],
     credentials: true,
   })
 );
